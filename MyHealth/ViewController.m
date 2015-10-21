d93a345c76ac6139465965a86004701054f932ac
imac <imac@imac13.local>
2015-10-21 17:05:22 +0800

    Initial Commit

---
+++ MyHealth/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyHealth
+//
+//  Created by imac on 15/10/21.
+//  Copyright (c) 2015年 imac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
